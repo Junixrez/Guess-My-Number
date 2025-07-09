422391267dc800429a747814ccb591cce38e6283
Junixrez <junixrez@gmail.com>
2025-07-09 09:28:05 +0300

    bug fix

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 "use strict";
 
-const answer = Math.floor(Math.random() * 10) + 1;
-console.log(answer);
+const secretNumber = Math.floor(Math.random() * 10) + 1;
+console.log(secretNumber);
 let lives = 3;
 
 document.querySelector(`.check`).addEventListener(`click`, function () {
-  const secretNumber = Number(document.querySelector(`.guess`).value);
+  const answer = Number(document.querySelector(`.guess`).value);
 
-  if (!secretNumber || secretNumber < 1 || secretNumber > 20) {
+  if (!answer || answer < 1 || answer > 10) {
     document.querySelector(
       `.message`
     ).textContent = `please choose a number from 1 to 10`;
-  } else if (secretNumber === answer) {
+  } else if (answer === secretNumber) {
     if (lives > 0) {
       document.querySelector(`.message`).textContent = `correct!!`;
-      document.querySelector(`.secret-number`).textContent = answer;
+      document.querySelector(`.secret-number`).textContent = secretNumber;
     } else {
       document.querySelector(
         `.message`
       ).textContent = `You lost press the Again! button to retry`;
     }
-  } else if (secretNumber > answer) {
+  } else if (answer > secretNumber) {
     if (lives > 0) {
       document.querySelector(`.message`).textContent = `TOO HIGH`;
       lives--;
@@ -36,7 +36,7 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
       });
       document.querySelector(`.label-score`).textContent = `💔 Lives: 0 `;
     }
-  } else if (secretNumber < answer) {
+  } else if (answer < secretNumber) {
     if (lives > 1) {
       document.querySelector(`.message`).textContent = `TOO LOW`;
       lives--;
